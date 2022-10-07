208c26a4751cebe53785d61b10c5b641b7145213
akotsampaseris <a.kotsampaseris@gmail.com>
2022-10-07 03:03:47 +0100

    Changed homepage

--- crypto_wallet_generator/urls.py
+++ crypto_wallet_generator/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('api/', include([
         path('', include('crypto_addresses.urls')),
         path('', include('crypto_coins.urls')),
