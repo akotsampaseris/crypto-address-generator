739d545b143763c2906fe4a1470f2b60346cc995
akotsampaseris <a.kotsampaseris@gmail.com>
2022-10-07 02:40:27 +0100

    Not using staticfiles app

--- crypto_wallet_generator/settings.py
+++ crypto_wallet_generator/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     
     # Custom project apps
     'crypto_addresses.apps.CryptoAddressesConfig',
