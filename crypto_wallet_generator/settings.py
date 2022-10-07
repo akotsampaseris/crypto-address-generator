0a4921186af65ee9d6ab89cec4a9908ea943c0b0
akotsampaseris <a.kotsampaseris@gmail.com>
2022-10-07 02:42:52 +0100

    Updated settings

--- crypto_wallet_generator/settings.py
+++ crypto_wallet_generator/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     
     # Custom project apps
     'crypto_addresses.apps.CryptoAddressesConfig',
